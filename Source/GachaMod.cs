5aca0d95ea755263d539414caf26cd1429d97a02
Sarah Pace <sarahpace37@gmail.com>
2022-09-08 20:59:48 -0700

    Initial commit

---
+++ Source/GachaMod.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace GachaMod
+{
+    public class GachaMod : Mod
+    {
+        public const string PACKAGE_ID = "gachamod.1trickPwnyta";
+        public const string PACKAGE_NAME = "Gacha Mod";
+
+        public GachaMod(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony(PACKAGE_ID);
+            harmony.PatchAll();
+
+            Log.Message($"[{PACKAGE_NAME}] Loaded.");
+        }
+    }
+}
